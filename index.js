a78242522fcf0b3dcc63d95196024c34b69d6bb9
Roberto <robertogarcia287@gmail.com>
2023-05-07 21:55:15 -0500

    Roberto's Modifying Dom Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+main.remove()
+
+let newHeader = document.createElement("h1");
+
+
+
+newHeader.id = "victory"
+
+newHeader.textContent = "Roberto is the champion"
+
+document.body.appen(newHeader);
+
+
+
+
+
+
+
